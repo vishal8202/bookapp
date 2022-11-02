94b59efdffb1546912fc6db1eb24d497f5f0800a
vishal8202 <111052930+vishal8202@users.noreply.github.com>
2022-11-02 21:48:12 +0530

    view

--- book_details.py
+++ book_details.py
@@ -36,23 +36,8 @@ while True:
         print('Sucessfully !!!!')
     elif(choice==3):
         print('search a book selected')
-        category = input('Enter the category of the book you needed : ')
-        sql = "SELECT `id`, `book_name`, `book_categ`, `charge_p_day`, `Author_name`, `Established_date` FROM `books_detail` WHERE `book_categ`='"+category+"'"
-        mycursor.execute(sql)
-        result = mycursor.fetchall()
-        print(result)
     elif(choice==4):
         print('update the book selected')
-        charge_p_day = input('Enter the price for each day to be get updated : ')
-        name = input('Enter the book name : ')
-        category = input('Enter the category of the book : ')
-       
-        AuthorName = input('Enter the author name : ')
-        established = input('Enter the date of established : ')
-        sql = "UPDATE `books_detail` SET `book_name`='"+name+"',`book_categ`='"+category+"',`charge_p_day`='"+charge_p_day+"',`Author_name`='"+AuthorName+"',`Established_date`='"+established+"' WHERE `charge_p_day`="+charge_p_day
-        mycursor.execute(sql)
-        mydb.commit()
-        print('Updated sucessfully !!!')
     elif(choice==5):
         print('delete the book selected')
     elif(choice==6):
